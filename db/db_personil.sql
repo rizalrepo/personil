387d95baf191fbd8ab08fd1bfaef126def91ccef
rizalrepo <rizalyusei@gmail.com>
2023-01-23 11:43:07 +0800

    up db

---
+++ db/db_personil.sql
@@ -0,0 +1,404 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2023 at 04:42 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_personil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuti`
+--
+
+CREATE TABLE `cuti` (
+  `id_cuti` int(11) NOT NULL,
+  `no_surat` varchar(30) DEFAULT NULL,
+  `tgl_surat` date NOT NULL,
+  `id_personil` int(11) NOT NULL,
+  `ket` text NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_selesai` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cuti`
+--
+
+INSERT INTO `cuti` (`id_cuti`, `no_surat`, `tgl_surat`, `id_personil`, `ket`, `tgl_mulai`, `tgl_selesai`) VALUES
+(3, '01/CUTI/I/2023', '2023-01-19', 2, 'test ket2', '2023-01-25', '2023-01-25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nm_jabatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jabatan`
+--
+
+INSERT INTO `jabatan` (`id_jabatan`, `nm_jabatan`) VALUES
+(3, 'PAUR 1 GADIK'),
+(4, 'PAMIN 1 GADIK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_kegiatan`
+--
+
+CREATE TABLE `jenis_kegiatan` (
+  `id_jenis_kegiatan` int(11) NOT NULL,
+  `nm_jenis_kegiatan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jenis_kegiatan`
+--
+
+INSERT INTO `jenis_kegiatan` (`id_jenis_kegiatan`, `nm_jenis_kegiatan`) VALUES
+(1, 'Kunjungan'),
+(3, 'Protokol');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id_kegiatan` int(11) NOT NULL,
+  `nm_kegiatan` varchar(100) NOT NULL,
+  `id_jenis_kegiatan` int(11) NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_selesai` date NOT NULL,
+  `tempat` varchar(100) NOT NULL,
+  `ket` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kegiatan`
+--
+
+INSERT INTO `kegiatan` (`id_kegiatan`, `nm_kegiatan`, `id_jenis_kegiatan`, `tgl_mulai`, `tgl_selesai`, `tempat`, `ket`) VALUES
+(1, 'Test Kegiatan', 3, '2023-01-20', '2023-01-21', 'Test Tempat', 'test ket');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mutasi`
+--
+
+CREATE TABLE `mutasi` (
+  `id_mutasi` int(11) NOT NULL,
+  `id_personil` int(11) NOT NULL,
+  `id_jabatan` int(11) NOT NULL,
+  `id_jabatan_lama` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `file_sk` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mutasi`
+--
+
+INSERT INTO `mutasi` (`id_mutasi`, `id_personil`, `id_jabatan`, `id_jabatan_lama`, `tanggal`, `file_sk`) VALUES
+(2, 2, 4, 3, '2023-01-21', '35743.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pangkat`
+--
+
+CREATE TABLE `pangkat` (
+  `id_pangkat` int(11) NOT NULL,
+  `nm_pangkat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pangkat`
+--
+
+INSERT INTO `pangkat` (`id_pangkat`, `nm_pangkat`) VALUES
+(2, 'KOMPOL'),
+(3, 'AKP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personil`
+--
+
+CREATE TABLE `personil` (
+  `id_personil` int(11) NOT NULL,
+  `nm_personil` varchar(50) NOT NULL,
+  `nrp_nip` varchar(50) NOT NULL,
+  `id_pangkat` int(11) NOT NULL,
+  `id_jabatan` int(11) NOT NULL,
+  `tmpt_lahir` varchar(50) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jk` varchar(20) NOT NULL,
+  `agama` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `hp` varchar(20) NOT NULL,
+  `tmt` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `personil`
+--
+
+INSERT INTO `personil` (`id_personil`, `nm_personil`, `nrp_nip`, `id_pangkat`, `id_jabatan`, `tmpt_lahir`, `tgl_lahir`, `jk`, `agama`, `alamat`, `hp`, `tmt`) VALUES
+(2, 'Budiman', '8989898888', 2, 4, 'Kapuas', '1990-01-15', 'Laki-laki', 'Islam', 'Martapura', '085248176794', '2019-01-19'),
+(3, 'Ade Setiadi', '8989898891', 3, 4, 'Balangan', '1993-01-10', 'Laki-laki', 'Islam', 'Banjarbaru', '087845783412', '2020-01-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tugas`
+--
+
+CREATE TABLE `tugas` (
+  `id_tugas` varchar(20) NOT NULL,
+  `no_surat` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `agenda` text NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_selesai` date NOT NULL,
+  `tempat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tugas`
+--
+
+INSERT INTO `tugas` (`id_tugas`, `no_surat`, `tgl_surat`, `agenda`, `tgl_mulai`, `tgl_selesai`, `tempat`) VALUES
+('63cb9b5b27fb6', '01/ST/I/2023', '2023-01-21', 'Pelatihan', '2023-01-23', '2023-01-24', 'Test Tempat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tugas_detail`
+--
+
+CREATE TABLE `tugas_detail` (
+  `id_tugas_detail` int(11) NOT NULL,
+  `id_tugas` varchar(20) NOT NULL,
+  `id_personil` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tugas_detail`
+--
+
+INSERT INTO `tugas_detail` (`id_tugas_detail`, `id_tugas`, `id_personil`) VALUES
+(1, '63cb9b5b27fb6', 3),
+(2, '63cb9b5b27fb6', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nm_user` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nm_user`, `username`, `password`, `level`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1'),
+(2, 'Kepala SPN', 'kaspn', '15c0cc318e80f603ec510a82bedc6d97', '2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD PRIMARY KEY (`id_cuti`),
+  ADD KEY `id_personil` (`id_personil`);
+
+--
+-- Indexes for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`id_jabatan`);
+
+--
+-- Indexes for table `jenis_kegiatan`
+--
+ALTER TABLE `jenis_kegiatan`
+  ADD PRIMARY KEY (`id_jenis_kegiatan`);
+
+--
+-- Indexes for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD PRIMARY KEY (`id_kegiatan`),
+  ADD KEY `id_jenis_kegiatan` (`id_jenis_kegiatan`);
+
+--
+-- Indexes for table `mutasi`
+--
+ALTER TABLE `mutasi`
+  ADD PRIMARY KEY (`id_mutasi`),
+  ADD KEY `id_personil` (`id_personil`),
+  ADD KEY `id_jabatan` (`id_jabatan`);
+
+--
+-- Indexes for table `pangkat`
+--
+ALTER TABLE `pangkat`
+  ADD PRIMARY KEY (`id_pangkat`);
+
+--
+-- Indexes for table `personil`
+--
+ALTER TABLE `personil`
+  ADD PRIMARY KEY (`id_personil`),
+  ADD KEY `id_pangkat` (`id_pangkat`),
+  ADD KEY `id_jabatan` (`id_jabatan`);
+
+--
+-- Indexes for table `tugas`
+--
+ALTER TABLE `tugas`
+  ADD PRIMARY KEY (`id_tugas`);
+
+--
+-- Indexes for table `tugas_detail`
+--
+ALTER TABLE `tugas_detail`
+  ADD PRIMARY KEY (`id_tugas_detail`),
+  ADD KEY `id_personil` (`id_personil`),
+  ADD KEY `tugas_detail_ibfk_3` (`id_tugas`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cuti`
+--
+ALTER TABLE `cuti`
+  MODIFY `id_cuti` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `jenis_kegiatan`
+--
+ALTER TABLE `jenis_kegiatan`
+  MODIFY `id_jenis_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `mutasi`
+--
+ALTER TABLE `mutasi`
+  MODIFY `id_mutasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `pangkat`
+--
+ALTER TABLE `pangkat`
+  MODIFY `id_pangkat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `personil`
+--
+ALTER TABLE `personil`
+  MODIFY `id_personil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tugas_detail`
+--
+ALTER TABLE `tugas_detail`
+  MODIFY `id_tugas_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD CONSTRAINT `cuti_ibfk_1` FOREIGN KEY (`id_personil`) REFERENCES `personil` (`id_personil`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD CONSTRAINT `kegiatan_ibfk_1` FOREIGN KEY (`id_jenis_kegiatan`) REFERENCES `jenis_kegiatan` (`id_jenis_kegiatan`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `mutasi`
+--
+ALTER TABLE `mutasi`
+  ADD CONSTRAINT `mutasi_ibfk_1` FOREIGN KEY (`id_personil`) REFERENCES `personil` (`id_personil`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mutasi_ibfk_2` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `personil`
+--
+ALTER TABLE `personil`
+  ADD CONSTRAINT `personil_ibfk_1` FOREIGN KEY (`id_pangkat`) REFERENCES `pangkat` (`id_pangkat`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `personil_ibfk_2` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tugas_detail`
+--
+ALTER TABLE `tugas_detail`
+  ADD CONSTRAINT `tugas_detail_ibfk_2` FOREIGN KEY (`id_personil`) REFERENCES `personil` (`id_personil`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
